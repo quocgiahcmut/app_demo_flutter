e86e3aea6f2cee1445776d8852a51c73062b59c1
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2025-05-06 00:19:19 +0700

    .

---
+++ android/app/src/main/kotlin/quocgia/mobile/app_demo_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package quocgia.mobile.app_demo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:app_demo_flutter/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(title: 'Home Screen'),
+    );
+  }
+}
